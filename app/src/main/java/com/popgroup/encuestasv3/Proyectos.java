32d698d0cbfdf32a01752434da56ea35a4b06370
jesus.hernandez <jamriohv@gmail.com>
2016-12-13 15:25:00 -0600

    Primer Commit 1.0

--- app/src/main/java/com/popgroup/encuestasv3/Proyectos.java
+++ app/src/main/java/com/popgroup/encuestasv3/Proyectos.java
@@ -1,11 +1,15 @@
 package com.popgroup.encuestasv3;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import com.j256.ormlite.android.apptools.OpenHelperManager;
+import com.j256.ormlite.dao.Dao;
 import com.popgroup.encuestasv3.DataBase.DBHelper;
 import com.popgroup.encuestasv3.Model.Proyecto;
 
@@ -28,14 +32,24 @@ public class Proyectos extends AppCompatActivity {
     String cliente;
 
     public DBHelper mDBHelper;
-
+    Dao dao;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activty_proyectos);
 
+        bundle = new Bundle();
 
-    }
+        adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,arrayProyectos){
+            @NonNull
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                return super.getView(position, convertView, parent);
+
+            }
+        };
+
+   }
     private DBHelper getmDBHelper() {
         if (mDBHelper == null) {
             mDBHelper = OpenHelperManager.getHelper(this, DBHelper.class);
