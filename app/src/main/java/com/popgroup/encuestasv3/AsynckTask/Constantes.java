c208b687051aa3d317b21ff912a8bf5909420650
orion <jmariohv@gmail.com>
2019-10-26 21:37:42 -0500

    se actualiza gradle y dependencias

--- app/src/main/java/com/popgroup/encuestasv3/AsynckTask/Constantes.java
+++ app/src/main/java/com/popgroup/encuestasv3/AsynckTask/Constantes.java
@@ -6,12 +6,15 @@ package com.popgroup.encuestasv3.AsynckTask;
  */
 
 public class Constantes {
-
-
+//http://info-sat.com.mx/
+/*
     public static String IPWBService = "http://popresearch8.cloudapp.net/b/acces2.php";
     public static String IPSetFoto = "http://popresearch8.cloudapp.net/b/fotosws.php";
     public static String IPSetEncuesta = "http://popresearch8.cloudapp.net/b/setEncuesta.php"; // todo solo prubea
 
+*/  public static String IPWBService = "http://info-sat.com.mx/b/acces2.php";
+    public static String IPSetFoto = "http://info-sat.com.mx/b/fotosws.php";
+    public static String IPSetEncuesta = "http://info-sat.com.mx/b/setEncuesta.php";
 
     public static String getIPWBService () {
         return IPWBService;
